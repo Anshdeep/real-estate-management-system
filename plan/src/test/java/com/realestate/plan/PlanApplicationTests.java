80a32344df978c75ae87e2b110202f3c06937c2f
Harshita Bilgaiyan <harshita@threadresearch.com>
2026-05-24 11:01:17 +0530

    First commit - Spring boot Setup

---
+++ plan/src/test/java/com/realestate/plan/PlanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.realestate.plan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
